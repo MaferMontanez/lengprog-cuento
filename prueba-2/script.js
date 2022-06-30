6e2d3bec728c8ceef3d9f8b3c4bef88af1f473bb
MaferMontanez <93534572+MaferMontanez@users.noreply.github.com>
2022-06-30 17:19:04 -0400

    Add files via upload

---
+++ prueba-2/script.js
@@ -0,0 +1,23 @@
+/*
+1.Crea una página web que facilite la creación colectiva de un cuento que quede 
+registrado en la página. Debe tener un input donde se pueda ir agregando texto 
+a un párrafo, sin borrar lo ya existente.
+
+El usuario también podrá ingresar, en otro input, el nombre del color que desee para
+el texto, entre rojo, azul, verde, amarillo, negro, violeta, naranjo o café, así 
+como también el color de fondo de todo el documento con estas mismas opciones.
+
+
+2.Añadir un formulario que capturé los siguientes datos del usuario que está creando
+el cuento: nombre, apellido, edad, ciudad y país a la misma página 
+
+Definir la diagramación libremente (formulario, input y botón para añadir párrafos y
+opciones para modificar color de elementos) con consideraciones de usabilidad y 
+adecuada distribución de los elementos.
+
+
+3.Crear un botón para previsualizar los datos capturados (formulario y el cuento.), 
+a través de una alerta.
+*/
+
+
